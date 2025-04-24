51c98403ab6c6156af5d3027a6282c599f828a7b
Crubel65 <Crubel8408@stu.d214.org>
2025-04-24 14:27:42 -0500

    Initial Commit

---
+++ ContentView.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State var rotation = 0
+    var body: some View {
+        VStack {
+           
+            ZStack {
+                Path { path in
+                    path.move(to: CGPoint(x: 75, y: 300))     // Top center
+                    path.addLine(to: CGPoint(x: 0, y: 150)) // Bottom left
+                    path.addLine(to: CGPoint(x: 150, y: 150)) // Bottom right
+                    path.closeSubpath()
+                }
+                Circle()
+                    .foregroundStyle(.white)
+                    .frame(width: 550, height: 550)
+                Circle()
+                    .fill(
+                        AngularGradient(
+                            gradient: Gradient(colors: [.red, .black, .red, .black, .red, .black, Color .red, .black, .red, .black, .red, .black, .red, .black, .red, .black, .red, .black, .red, .black, .red, .black, .red, .black, .green,]),
+                            center: .center))
+                    .frame(width: 500, height: 500)
+                    .onTapGesture{
+                    }
+                Circle()
+                    .foregroundStyle(.yellow)
+                    .frame(width: 100, height: 100)
+                Circle()
+                    .foregroundStyle(.brown)
+                    .frame(width: 50, height: 50)
+                
+            }
+        }
+    }
+}
